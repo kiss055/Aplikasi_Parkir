8fc484cc2c9be859592deb9f386dc70098f6e42d
kiss055 <bilkisauliyaagustin@gmail.com>
2026-04-09 08:55:12 +0700

    config.php

--- Controller/config.php
+++ Controller/config.php
@@ -1,14 +1,9 @@
-<?php
-//konfigurasi koneksi ke database,digunakan oleh seluruh file di dalam aplikasi
-
+<<?php
 date_default_timezone_set('Asia/Jakarta');
-//berfungsi menyamakan jam sistem PHP dengan jam lokal Indonesia, supaya semua proses waktu di aplikasi akurat dan konsisten.
 
-$conn = mysqli_connect("127.0.0.1","root","root","aplikasi_parkir");
-// koneksi ke database
+$conn = mysqli_connect("localhost","root","","aplikasi_parkir");
 
 if(!$conn){
-    die("Koneksi database gagal!");
-    //mengecek apakah koneksi berhasil
+    die("Koneksi database gagal: " . mysqli_connect_error());
 }
 ?>
